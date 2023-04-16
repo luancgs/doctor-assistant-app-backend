bed41f84efd5f09eece096766e5ad8259835ce6f
Luan <luan.carlos.3174@gmail.com>
2023-04-16 15:41:08 -0300

    feat: :card_file_box: adds exams and prescriptions to database model

--- database/models/models.go
+++ database/models/models.go
@@ -8,35 +8,40 @@ import (
 )
 
 type Doctor struct {
-	ID           uint
-	Name         string
-	Email        string
-	Password     string
-	MedicalCode  sql.NullString
-	Appointments []Appointment
-	CreatedAt    time.Time
-	UpdatedAt    time.Time
-	DeletedAt    gorm.DeletedAt `gorm:"index"`
+	ID            uint
+	Name          string         `json:"name"`
+	Email         string         `json:"email"`
+	Password      string         `json:"password"`
+	MedicalCode   sql.NullString `json:"medical_code"`
+	Appointments  []Appointment  `json:"appointments"`
+	Exams         []Exam         `json:"exams"`
+	Prescriptions []Prescription `json:"prescriptions"`
+	CreatedAt     time.Time
+	UpdatedAt     time.Time
+	DeletedAt     gorm.DeletedAt `gorm:"index"`
 }
 
 type Patient struct {
-	ID           uint
-	Name         string    `json:"name"`
-	Email        string    `json:"email"`
-	Password     string    `json:"password"`
-	Age          uint8     `json:"age"`
-	Birthday     time.Time `json:"birthday"`
-	Sex          string    `json:"sex"`
-	Appointments []Appointment
-	CreatedAt    time.Time
-	UpdatedAt    time.Time
-	DeletedAt    gorm.DeletedAt `gorm:"index"`
+	ID            uint
+	Name          string         `json:"name"`
+	Email         string         `json:"email"`
+	Password      string         `json:"password"`
+	Age           uint8          `json:"age"`
+	Birthday      time.Time      `json:"birthday"`
+	Sex           string         `json:"sex"`
+	Appointments  []Appointment  `json:"appointments"`
+	Exams         []Exam         `json:"exams"`
+	Prescriptions []Prescription `json:"prescriptions"`
+	CreatedAt     time.Time
+	UpdatedAt     time.Time
+	DeletedAt     gorm.DeletedAt `gorm:"index"`
 }
 
 type Appointment struct {
 	ID        uint
-	Title     string
-	Date      time.Time
+	Title     string    `json:"title"`
+	Date      time.Time `json:"date"`
+	Status    uint      `json:"status"`
 	PatientID uint
 	DoctorID  uint
 	CreatedAt time.Time
@@ -44,4 +49,26 @@ type Appointment struct {
 	DeletedAt gorm.DeletedAt `gorm:"index"`
 }
 
-// TODO: add exam, prescription, document and payment structures
+type Exam struct {
+	ID        uint
+	Title     string    `json:"title"`
+	Date      time.Time `json:"date"`
+	Status    uint      `json:"status"`
+	Result    string    `json:"result"`
+	PatientID uint
+	DoctorID  uint
+	CreatedAt time.Time
+	UpdatedAt time.Time
+	DeletedAt gorm.DeletedAt `gorm:"index"`
+}
+
+type Prescription struct {
+	ID              uint
+	Title           string    `json:"title"`
+	Expiration_date time.Time `json:"expiration_date"`
+	PatientID       uint
+	DoctorID        uint
+	CreatedAt       time.Time
+	UpdatedAt       time.Time
+	DeletedAt       gorm.DeletedAt `gorm:"index"`
+}
